08cf7ecd5bd23e4a21639271a326dc894c2cc304
cstung21 <cstung21@gmail.com>
2018-07-31 11:20:59 -0700

    Complete identity through uniq

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,12 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else if (n > array.length) {
+      return array;
+    }
+    return n === undefined ? array[array.length - 1] : array.slice(-n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +52,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +83,40 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var trueElements = [];
+    _.each(collection, function(value) {
+      if (test(value)) {
+        trueElements.push(value);
+      }
+    });
+    return trueElements;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(value) {
+      return (!test(value));
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+
+    iterator = iterator === undefined ? _.identity : iterator;
+    var processedArray = [];
+    var uniqValues = {};
+    _.each(array, function(value) {
+      if(!uniqValues.hasOwnProperty(iterator(value))) {
+        uniqValues[iterator(value)] = value
+      }
+      
+    })
+    for (var key in uniqValues) {
+      processedArray.push(uniqValues[key]);
+    }
+    return processedArray;
   };
 
 
